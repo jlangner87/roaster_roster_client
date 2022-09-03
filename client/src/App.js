62ba6b866bdd6cf795fbb5b853646687a7cd5cf6
jlangner87 <josh.langner@gmail.com>
2022-09-02 23:49:45 -0500

    restructure files for React components to import

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import BeanForm from './components/BeanForms'
 
 function App() {
   return (
     <div className="App">
       <h1>This is MY site now!</h1>
+      <BeanForm />
     </div>
   )
 }
---
+++ client/src/components/BeanForms.jsx
@@ -0,0 +1,10 @@
+const BeanForm = () => {
+
+  return (
+    <div className="bean_form">
+      <h1>This is my bean form component!</h1>
+    </div>
+  )
+}
+
+export default BeanForm
