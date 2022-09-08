593a471ac459004d2ba311438bf687c7276ea1b2
jlangner87 <josh.langner@gmail.com>
2022-09-08 15:19:16 -0500

    first useeffect on beans list fires console log when page loads.

--- client/src/pages/beans.js
+++ client/src/pages/beans.js
@@ -1,11 +1,17 @@
-import BeanProfile from '../components/BeanProfile'
+import { BASE_URL } from '../globals'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
-const Beans = () => {
-  return (
-    <div className="Beans">
-      <BeanProfile />
-    </div>
-  )
+const Beans = (props) => {
+  useEffect(() => {
+    const listBeans = async () => {
+      console.log('Beans Beans Beans!')
+    }
+    listBeans()
+  })
+
+  return <div className="all_beans"></div>
 }
 
 export default Beans
