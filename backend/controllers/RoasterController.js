a03f336f57aa5aaccfafd43a8d4fe351d4a18133
jlangner87 <josh.langner@gmail.com>
2022-09-09 17:25:51 -0500

    get roaster by id express route returns expected json on insomnia test

--- backend/controllers/RoasterController.js
+++ backend/controllers/RoasterController.js
@@ -5,6 +5,15 @@ const GetRoasters = async (req, res) => {
   res.send(all)
 }
 
+const GetOneRoaster = async (req, res) => {
+  let roasterId = parseInt(req.params.roasters_id)
+  let desiredRoaster = await Roaster.findOne({
+    where: { id: roasterId }
+  })
+  res.send(desiredRoaster)
+}
+
 module.exports = {
-  GetRoasters
+  GetRoasters,
+  GetOneRoaster
 }
--- backend/routes/RoasterRouter.js
+++ backend/routes/RoasterRouter.js
@@ -2,5 +2,6 @@ const Router = require('express').Router()
 const controller = require('../controllers/RoasterController')
 
 Router.get('/all', controller.GetRoasters)
+Router.get('/:roasters_id', controller.GetOneRoaster)
 
 module.exports = Router
